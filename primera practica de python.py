e0cfa996f3938eef8e4e158e74e8d1ed973347d3
Machillo <gatotico99@gmail.com>
2021-04-15 13:59:15 -0600

    Primera commit de practica

---
+++ primera practica de python.py
@@ -0,0 +1,9 @@
+Name= input("Escribe tu nombre aquí...")
+
+Direction= input("Escribe tu dirección aquí...")
+
+Phone= input("Escribe tu número telefonico aquí...")
+
+listas_de_datos=[Name, Direction, Phone]
+
+print("Los datos perosnales son: " + listas_de_datos[0] + " " + listas_de_datos[1] + " " + listas_de_datos[2])
