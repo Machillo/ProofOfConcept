248d1533e187b48991aa4964f03052d5dbd54f9a
Machillo <gatotico99@gmail.com>
2021-05-06 11:40:01 -0600

    commit

--- Spikes/Trivial/Function.js
+++ Spikes/Trivial/Function.js
@@ -16,32 +16,9 @@ function CheckAnswer(){
                }
            }
     }
-    picture();
-
+    
     var Outcome = document.getElementById('Outcome');
-    Outcome.innerHTML = '<h3>You got <span> '+Points+' </span> of <span> '+Total+' points</span></h3>';
-    
-    if (Outcome === 3){
-        window.alert(function picture(){ 
-            var pic = "Images/Ibba.png"
-            document.getElementById("Image").src = pic;
-            document.getElementById("Image").style.display='block';
-            });
-   } else if (Outcome === 2){
-        window.alert(function picture(){
-            var pic1 = "Images/Homero.png"
-            document.getElementById("Image").src = pic1;
-            document.getElementById("Image").style.display= 'block';
-            });
-   } else{
-       window.alert(function picture(){ 
-        var pic2 = "Images/cat_craying.jpg"
-        document.getElementById("Image").src = pic2;
-        document.getElementById("Image").style.display='block';
-        });
-   }   
-
-    
+    Outcome.innerHTML = '<h3>You got <span> '+Points+' </span> of <span> '+Total+' points</span></h3>'; 
 
     return false;
 
