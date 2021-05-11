23f795e33987fae432dec3a7202e7829c97131a9
Machillo <gatotico99@gmail.com>
2021-05-11 10:52:04 -0600

    change value

--- Spikes/Trivial/Function.js
+++ Spikes/Trivial/Function.js
@@ -16,21 +16,7 @@ function CheckAnswer(){
                }
            }
     }
-    picture();
-
-    if(Points[3].value === 3){function picture(){ 
-        var pic = "Images/Ibba.png"
-        document.getElementById("Image").src = pic;
-        document.getElementById("Image").style.display='block';
-        }
-    }else{
-        if(Points[2] === 2){function picture(){
-            var pic1 = "Images/Homero.png"
-            document.getElementById("Image").src = pic1;
-            document.getElementById("Image").style.display= 'block';
-            }}
-    }
-
+    
     var Outcome = document.getElementById('Outcome');
     Outcome.innerHTML = '<h3>You got <span> '+Points+' </span> of <span> '+Total+' points</span></h3>'; 
 
