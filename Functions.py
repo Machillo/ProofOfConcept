993883f3b50771d8217e7fc2f6541574b74f95f4
Kenneth Alvarado <gatotico99@gmail.com>
2021-08-31 14:45:42 -0600

    message commit

--- Functions.py
+++ Functions.py
@@ -2,6 +2,10 @@ def print_message():
     print('Mensaje especial: ')
     print('¡Estoy aprendiendo a usar funciones!')
 
+print_message()
+print_message()
+print_message()
+
 """print('Mensaje especial: ')
 print('¡Estoy aprendiendo a usar funciones!')
 print('Mensaje especial: ')
