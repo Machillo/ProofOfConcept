f915bc0bf204e402860704627b60df62a179aec4
Kenneth Alvarado <gatotico99@gmail.com>
2021-09-02 11:21:56 -0600

    game.py_commit

---
+++ TextStrings.py
@@ -0,0 +1,5 @@
+.upper() #te pone el texto en mayúscula
+.capitalize() #te pone la primera letra en mayúscula
+.strip() #te quita los espacios
+.lower() #te pone el texto en minúscula
+.replace() #te cambia de lugar los carecteres deseados, ejem: nombre = nombre.repace ('e', 'a') de Kenneth pasa a Kannath
